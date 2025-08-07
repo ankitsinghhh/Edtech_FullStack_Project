f50ca068ffebbe10ba2277eb7d32daec0f5650f3
ankitsinghhh <ankitsingh79834@gmail.com>
2025-08-07 18:50:42 +0530

    fixing cors error

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-     origin:"http://localhost:3000",
+     origin:"https://edtech-fullstack-project-backend.onrender.com",
      credentials:true,
 }))
 
